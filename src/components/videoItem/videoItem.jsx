dd94929b293ba2ab64876256eb3912c5079a2070
lsh <sermyes@gmail.com>
2021-02-12 02:35:57 +0900

    remove warning && update musicCenter

--- src/components/videoItem/videoItem.jsx
+++ src/components/videoItem/videoItem.jsx
@@ -1,39 +1,13 @@
-import React, { memo, useEffect, useRef, useCallback, useState } from "react";
+import React, { memo } from "react";
 import styles from "./videoItem.module.css";
 
 const VideoItem = memo(({ video, onVideoClick }) => {
-  const videoRef = useRef();
-  const [loading, setLoading] = useState(false);
   const onClick = () => {
     onVideoClick(video);
   };
 
-  const videoHeightHandler = useCallback(() => {
-    const width = videoRef.current.offsetWidth;
-    if (width <= 250) {
-      videoRef.current.style.cssText = `max-height: 220px`;
-    } else if (width <= 320) {
-      videoRef.current.style.cssText = `max-height: 260px`;
-    } else {
-      videoRef.current.style.cssText = `max-height: 300px`;
-    }
-  }, []);
-
-  useEffect(() => {
-    if (!loading) {
-      videoHeightHandler();
-      setLoading(true);
-    }
-
-    window.addEventListener("resize", videoHeightHandler);
-
-    return () => {
-      window.removeEventListener("resize", videoHeightHandler);
-    };
-  }, [videoHeightHandler, loading]);
-
   return (
-    <div ref={videoRef} className={styles.video} onClick={onClick}>
+    <div className={styles.video} onClick={onClick}>
       <img
         className={styles.thumbnail}
         src={video.snippet.thumbnails.medium.url}
