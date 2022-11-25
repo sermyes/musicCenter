6eb1ece5cbe89978ac8134921e87a07f45af953b
lsh <sermyes@gmail.com>
2022-11-25 21:37:34 +0900

    modify form design

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -1,18 +1,18 @@
-class Youtube{
-    constructor(client){
-        this.client = client;
-    }
-    async playList(){
-        const response = await this.client.get("playlistItems", {
-            params: {
-              part: "snippet",
-              playlistId: "PL4fBVwST-63oKnpvGbt0Ok7Qkh3P22Plm",
-              maxResults: 25,
-            },
-          });
-          
-          return response.data.items;
-    }
+class Youtube {
+  constructor(client) {
+    this.client = client;
+  }
+  async playList() {
+    const response = await this.client.get('playlistItems', {
+      params: {
+        part: 'snippet',
+        playlistId: 'PL4fBVwST-63oKnpvGbt0Ok7Qkh3P22Plm',
+        maxResults: 40
+      }
+    });
+
+    return response.data.items;
+  }
 }
 
 export default Youtube;
